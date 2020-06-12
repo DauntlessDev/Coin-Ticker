de8ca6c79053c437908624ce174e0df60b6d9baa
Brave Leuterio <you@example.com>
2020-06-12 14:54:49 +0800

    finished project

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,31 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apikey = "20562BC7-5687-43F1-A7C1-1B61D5037508";
+const coinURL = "https://rest.coinapi.io/v1/exchangerate/";
+
+class CoinData {
+  Future<dynamic> getExchangeRate(String selectedCurrency, String crypto) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$coinURL$crypto/$selectedCurrency/?apikey=$apikey');
+
+    return networkHelper.getData();
+  }
+}
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+  final String url;
+
+  Future<dynamic> getData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+      return null;
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,76 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  CoinData coinData = CoinData();
+
+  int roundedBtc;
+  int roundedEth;
+  int roundedLtc;
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI();
+  }
+
+  Future<void> updateUI() async {
+    var btcRateData = await coinData.getExchangeRate(selectedCurrency, 'BTC');
+    var ethRateData = await coinData.getExchangeRate(selectedCurrency, 'ETH');
+    var ltcRateData = await coinData.getExchangeRate(selectedCurrency, 'LTC');
+
+    if (btcRateData != null && ltcRateData != null && ethRateData != null) {
+      setState(() {
+        double btcRate = btcRateData['rate'];
+        roundedBtc = btcRate.toInt();
+        double ltcRate = ltcRateData['rate'];
+        roundedLtc = ltcRate.toInt();
+        double ethRate = ethRateData['rate'];
+        roundedEth = ethRate.toInt();
+      });
+      return;
+    }
+  }
+
+  DropdownButton<String> getDropDownButton() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(child: Text(currency), value: currency);
+      dropDownItems.add(newItem);
+    }
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: dropDownItems,
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value;
+            updateUI();
+            print(selectedCurrency);
+          });
+        });
+  }
+
+  CupertinoPicker getIosPicker() {
+    List<Text> cupertinoItems = [];
+
+    for (String currency in currenciesList) {
+      var newItem =
+          Text(currency, style: TextStyle(color: Colors.white, fontSize: 17));
+      cupertinoItems.add(newItem);
+    }
+
+    return CupertinoPicker(
+        backgroundColor: Colors.lightBlue,
+        itemExtent: 20.0,
+        onSelectedItemChanged: (int value) {
+          setState(() {
+            selectedCurrency = currenciesList[value];
+            updateUI();
+          });
+        },
+        children: cupertinoItems);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,39 +87,93 @@ class _PriceScreenState extends State<PriceScreen> {
         title: Text('🤑 Coin Ticker'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Column(
+                children: <Widget>[
+                  Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 28.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            '1 BTC = $roundedBtc $selectedCurrency',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                  Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 28.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            '1 ETH = $roundedEth $selectedCurrency',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 28.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            '1 LTC = $roundedLtc $selectedCurrency',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
-        ],
-      ),
+            Container(
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? getIosPicker() : getDropDownButton(),
+            ),
+          ]),
     );
   }
 }
